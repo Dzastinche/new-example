596b282f192feef78d869693d65e127cf3c61cb6
Jasmin <jasmindelic94@outlook.com>
2022-02-15 11:36:43 +0100

    fix

--- livescore/src/redux/actions/actions.js
+++ livescore/src/redux/actions/actions.js
@@ -34,7 +34,11 @@ export const Newsfor=()=>{
     return(dispatch)=>{;
         const proxy=`https://cors-anywhere.herokuapp.com/`
         fetch(`${proxy}https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=c8ca03f400434aa49ac99176e8f8eb03`,{
-            'Access-Control-Allow-Origin':'http://localhost:3000'
+            'method':'GET',
+            "headers":{
+                'Access-Control-Allow-Origin':'http://localhost:3000'
+            }
+        
         })
         .then(el=>el.json())
         .then(ele=>{dispatch(addnews(ele.articles));dispatch(finishedNews());console.log(ele)})
